13db9af3f09918bec21ce7b706cd7f39ecd21e39
Alon447 <alon447@gmail.com>
2023-03-18 14:30:19 +0200

    made a few changes

--- Cars.py
+++ Cars.py
@@ -2,10 +2,13 @@
 
 
 class Cars:
-    def __init__(self, id, route,next_node_index):
+    def __init__(self, id, route,next_node_index, is_finished, travel_time):
         self.id = id
         self.route = route
-        self.nextNode = route[0]
+        self.next_node_index = route[0] # we need to decide if this parameter is a node or an index of a node in the route
+        self.is_finished = False
+        self.travel_time = 0
+        #  MAYBE WE NEED THE CARS TO STORE THE CURRENT EDGE AND THE NEXT EDGE. THE CURRENT EDGE TO CALCULATE THE TRAVEL TIME AND THE NEXT EDGE TO ASK TO MOVE TO THE EDGE IN THE NEXT TICK.
 
     def get_id(self):
         return self.id
@@ -16,13 +19,27 @@ class Cars:
     def get_next_node_index(self):
         return self.next_node_index
 
+    def get_travel_time(self):
+        return self.travel_time
+
+    def is_finished(self):
+        return self.is_finished
+
     def set_next_node_index(self, next_node_index):
         self.next_node_index = next_node_index
 
     def set_route(self, route):
         self.route = route
 
+    def set_is_finished(self):
+        if len(self.route)==1: # means that the car is in the last node of the route
+            is_finished=True # after the car is finished i think we need to remove it
+
+    def update_travel_time(self):
+        self.travel_time += ??
+
     def move_node(self):
-        self.next_node_index += 1
+        self.next_node_index += 1 # I think this is a mistake, we need to move the car to the next node in the route, not to the next index in the route
+
     def __str__(self):
         return "id: " + self.id + "\n" + "route: " + str(self.route) + "\n" + "next node: " + str(self.route[self.next_node_index])
--- QueuedEdge.py
+++ QueuedEdge.py
@@ -2,12 +2,14 @@ import itertools
 import copy
 class QueuedEdge:
 
-    def __init__(self, edge_graph_index, max_speed, cur_speed, reversed, cars_queue, next_queue):
+    def __init__(self, edge_graph_index, max_speed, cur_speed, reversed, cars_queue, next_queue, max_length):
         self.edge_graph_index = edge_graph_index
         self.max_speed = max_speed
         self.cur_speed = cur_speed
         self.reversed = reversed
         self.cars_queue = cars_queue #actually a list that will function similarly to a qeueu
+        self.next_queue = cars_queue
+        self.max_length = max_length # we need to define a mex length for the queue
 
     def get_edge_graph_index(self):
         return self.edge_graph_index
@@ -20,6 +22,7 @@ class QueuedEdge:
 
     def get_num_cars(self):
         return len(self.cars_queue)
+
     def is_reversed(self):
         return self.reversed
 
@@ -40,6 +43,13 @@ class QueuedEdge:
 
     def remove_cars(self, num_cars):
         self.next_queue = self.cars_queue[num_cars:]
+
+    def update(self):
+        self.cars_queue = self.next_queue
+        self.next_queue = copy.copy(self.cars_queue)
+        # NOW WE NEED TO UPDATE THE SPEED ACCORDING TO THE QUEUE
+        self.cur_speed = ??
+
     def __str__(self):
         return "edge_graph_index: " + str(self.edge_graph_index) + "\n" + "max_speed: " \
             + str(self.max_speed) + "\n" + "cur_speed: " + str(self.cur_speed) + "\n" + "reversed: " \
