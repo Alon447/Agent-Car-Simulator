62cbe4aec8c705211a6d7fd18378d3bde5fc0ecb
Alon447 <alon447@gmail.com>
2023-07-31 23:22:12 +0300

    now q learning find route between two points

--- new_master/Q_Learning_Functions.py
+++ new_master/Q_Learning_Functions.py
@@ -79,8 +79,9 @@ class QLearning:
 
 
     def train(self, num_episodes: int, car: Car, max_steps_per_episode=100, epsilon_decay_rate=0.99, mean_rewards_interval=10):
-        print("************************************************************************************")
-        print("Training started")
+        # print("*********************************************")
+        # print("          Training Started                   ")
+        # print("*********************************************")
 
         src = car.get_source_node()
         dst = car.get_destination_node()
@@ -131,23 +132,23 @@ class QLearning:
                 # print("total episode reward: ", total_episode_reward)
                 mean_reward = mean_reward_sum / mean_rewards_interval
                 mean_rewards.append(mean_reward)
-                if mean_reward > 960:
-                    # print("mean reward: ", mean_reward)
-                    # print("convrged after ", episode, " episodes")
-                    break
+                # if mean_reward > 960:
+                #     # print("mean reward: ", mean_reward)
+                #     # print("convrged after ", episode, " episodes")
+                #     break
                 mean_reward_sum = 0  # Reset the sum for the next 10 episodes
 
             path_roads.clear()
             path_nodes.clear()
             total_episode_reward = 0
             self.epsilon *= epsilon_decay_rate
+        #
+        # print("*********************************************")
+        # print("          Training Finished                  ")
+        # print(f"Source: {src}, Destination: {dst}")
+        # print("*********************************************")
 
-        print("************************************************************************************")
-        print("Training finished")
-        print("src: ", src, "dst: ", dst)
-        print("************************************************************************************")
-
-        # # Plot mean rewards
+        # Plot mean rewards
         # plt.plot(range(1, len(mean_rewards) + 1), mean_rewards)
         # plt.xlabel('Interval (Every 10 Episodes)')
         # plt.ylabel('Mean Episode Reward')
@@ -155,15 +156,17 @@ class QLearning:
         # plt.show()
 
     def test(self,car:Car, max_steps_per_episode=100):
-        print("************************************************************************************")
-        print("Testing started")
         src = car.get_source_node()
         dst = car.get_destination_node()
-        print("src: ", src, "dst: ", dst)
+        # print("*********************************************")
+        print("          Testing Started                    ")
+        print(f"Source: {src}, Destination: {dst}")
+        print("*********************************************")
+
 
 
         test_rewards = 0
-        path_nodes = []
+        path_nodes = [src]
         path_roads = []
         # car = Car(1, 1, 500, datetime.datetime.now(), self.road_network)
         state = car.get_source_node()
@@ -176,12 +179,17 @@ class QLearning:
             path_roads.append(next_road.get_id())
             path_nodes.append(next_road.get_destination_node())
             if reward == -100 or reward == 1000:  # car is blocked or reached destination
+                if reward == -100:
+                    print("Car is blocked!")
+                else:
+                    print("Car reached the destination!")
                 break
 
             state = next_state
         print("path nodes: ", path_nodes)
-        print("path roads: ", path_roads)
-        print("Test reward: ", test_rewards)
+        # print("path roads: ", path_roads)
+        print(f"Test reward: {test_rewards:.2f}")
+        print("*********************************************")
         return test_rewards
     def save_q_table(self, filename):
         np.save(filename, self.q_table)
@@ -189,20 +197,4 @@ class QLearning:
     def load_q_table(self, filename):
         self.q_table = np.load(filename)
 
-# Example usage
-# road_network = Road_Network("../data/TLV_with_eta.graphml")  # Replace with the correct path to your graphml file
-# q_learning = QLearning(road_network)
-#
-# # Train the Q-learning model
-# num_episodes = 100
-# cars=[]
-# c1=Car(1,1,2,datetime.datetime.now(),road_network)
-# cars.append(c1)
-# q_learning.train(num_episodes,cars)
-#
-# # Save the learned Q-table for future use
-# q_learning.save_q_table("q_table.npy")
-#
-# # Load the pre-trained Q-table from file
-# q_learning.load_q_table("q_table.npy")
-# Create an instance of the QLearningAgent
+
--- new_master/Q_Learning_Main.py
+++ new_master/Q_Learning_Main.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 
 from new_master.Car import Car
 from new_master.Road_Network import Road_Network
-from new_master.q_learning_new import QLearning
+from new_master.Q_Learning_Functions import QLearning
 
 def choose_random_src_dst(road_network):
     src = random.Random().randint(0, len(road_network.get_node_connectivity_dict()) - 1)
@@ -14,14 +14,14 @@ def choose_random_src_dst(road_network):
     src_osm = road_network.reverse_node_dict[src]
     dest_osm = road_network.reverse_node_dict[dst]
     while not nx.has_path(road_network.get_graph(), src_osm, dest_osm):
-        print(f"There is no path between {src} and {dst}.")
+        # print(f"There is no path between {src} and {dst}.")
         src = random.Random().randint(0, len(road_network.get_node_connectivity_dict()) - 1)
         dst = random.Random().randint(0, len(road_network.get_node_connectivity_dict()) - 1)
         src_osm = road_network.reverse_node_dict[src]
         dest_osm = road_network.reverse_node_dict[dst]
     return src, dst
 
-def test(road_network, number_of_tests = 100):
+def test(road_network, number_of_tests = 1):
     """
     :param road_network:
     :param number_of_tests:
@@ -29,13 +29,16 @@ def test(road_network, number_of_tests = 100):
     """
     test_rewards = []
     agent = QLearning(road_network, learning_rate=0.1, discount_factor=0.9, epsilon=0.1)
-    for _ in range(number_of_tests):
+    for i in range(number_of_tests):
+        print("test number: ", i)
         src, dst = choose_random_src_dst(road_network)
-        print("src: ", src, "dst: ", dst)
+        src= 455
+        dst = 516
+        # print("src: ", src, "dst: ", dst)
 
         c1 = Car(1, src, dst, datetime.datetime.now(), road_network)
         # Train the agent
-        num_episodes = 1500
+        num_episodes = 3000
         max_steps_per_episode = 100
         agent.train(num_episodes, c1, max_steps_per_episode=max_steps_per_episode)
         test_reward = agent.test(c1)  # this will be the test function
@@ -44,12 +47,15 @@ def test(road_network, number_of_tests = 100):
         else:
             test_rewards.append(0)
         # test_rewards.append(test_reward)
-    percentage = 100*sum(test_rewards) / number_of_tests
-    print("percentage: ", percentage)
+        percentage = 100 * sum(test_rewards) / len(test_rewards)
+        print("percentage: ", percentage)
 
 
 road_network = Road_Network("/TLV_with_eta.graphml")  # Replace with the correct path to your graphml file
-test(road_network, number_of_tests=5)
+test(road_network, number_of_tests=1000)
+
+
+
 # agent = QLearning(road_network, learning_rate=0.1, discount_factor=0.9, epsilon=0.1)
 # src = random.Random().randint(0, len(road_network.get_node_connectivity_dict()) - 1)
 # dst = random.Random().randint(0, len(road_network.get_node_connectivity_dict()) - 1)
